1942da5fb0803ea33acb7a14b370b769bbb154e7
Pranilak <aneesh.prabu@gmail.com>
2022-12-07 19:33:25 -0500

    Initial Commit

---
+++ MongoGPT3Demo/MongoGPT3DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  MongoGPT3DemoApp.swift
+//  MongoGPT3Demo
+//
+//  Created by Aneesh Prabu on 12/7/22.
+//
+
+import SwiftUI
+
+@main
+struct MongoGPT3DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
