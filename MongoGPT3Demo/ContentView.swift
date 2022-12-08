93668146fc14c773f8ddb54e473a7fbe27f629c5
Pranilak <aneesh.prabu@gmail.com>
2022-12-07 21:04:24 -0500

    Added login page

--- MongoGPT3Demo/ContentView.swift
+++ MongoGPT3Demo/ContentView.swift
@@ -8,12 +8,39 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var mongoURI: String = String()
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            
+            Image("gpt3").resizable().aspectRatio(contentMode: .fit).padding(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/).frame(width: /*@START_MENU_TOKEN@*/250.0/*@END_MENU_TOKEN@*/, height: /*@START_MENU_TOKEN@*/250.0/*@END_MENU_TOKEN@*/)
+            Spacer()
+            VStack(spacing:-10) {
+                Text("URI").bold().multilineTextAlignment(.leading).padding(.trailing, 270.0)
+                TextField("mongodb://localhost:27017", text: $mongoURI)
+                                        .padding()
+                                        .overlay(RoundedRectangle(cornerRadius: 10.0).strokeBorder(Color("TextColor"), style: StrokeStyle(lineWidth: 1.0)))
+                                        .padding()
+            }
+            
+            Spacer()
+            Button(action: {
+                print("sign up bin tapped")
+            }) {
+                Text("Connect")
+                    .frame(minWidth: 0, maxWidth: 150)
+                    .font(.system(size: 18))
+                    .padding()
+                    .foregroundColor(Color("TextColor"))
+                    .overlay(
+                        RoundedRectangle(cornerRadius: 10)
+                            .stroke(Color("MongoGreen"), lineWidth: 3)
+                )
+            }
+            .background(Color.white)
+            .cornerRadius(9)
+            Spacer()
         }
         .padding()
     }
